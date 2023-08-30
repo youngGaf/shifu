3951c0a71c29f501250b253fbf1a139afbe6f9cd
youngGaf <youngabuhafiz@gmail.com>
2023-08-30 10:30:12 -0700

    setup: backend structure

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,6 +3,9 @@ import express from "express";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
+import { itemsRouter } from "./items/items.router";
+import { errorHandler } from "./middleware/error.middleware";
+import { notFoundHandler } from "./middleware/not-found.middleware";
 
 
 dotenv.config();
@@ -20,6 +23,18 @@ app.use(morgan('tiny'));
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
+
+/**
+ * Routing
+ */
+ app.use("/api/menu/items", itemsRouter);
+
+/**
+ * Error handling
+ */
+app.use(errorHandler);
+app.use(notFoundHandler);
+
 
 /* Server Activation */
 app.listen(PORT, () =>{
---
+++ server/src/items/items.interface.ts
@@ -0,0 +1,6 @@
+import { Item } from "./item.interface";
+
+/* Objects of Items */
+export interface Items{
+    [key: number]: Item;
+}
