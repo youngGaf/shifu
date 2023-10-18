2d17b3b11312bb09a6a84d2ee4df163c97022d7a
youngGaf <youngabuhafiz@gmail.com>
2023-10-17 23:59:11 -0700

    setup: setup client for app

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./components/Header";
+
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
